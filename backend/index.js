18ea3dbd5d3fdc24ce3143852fe1bbcea1184dbf
PrithiviPM2580 <prithivipemimagar>
2025-10-08 02:31:11 +0545

    feat: initialize backend with package.json and MongoDB connection setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
